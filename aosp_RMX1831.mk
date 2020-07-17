d9a1e591aab64388d4f832ffc461d24d04d6479c
devil-black-786 <60068093+devil-black-786@users.noreply.github.com>
2020-07-17 11:18:01 +0530

    Update aosp_RMX1831.mk

--- aosp_RMX1831.mk
+++ aosp_RMX1831.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from realme RMX1831
 $(call inherit-product, device/realme/RMX1831/device.mk)
